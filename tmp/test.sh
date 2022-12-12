cdd5f6da443bad8c3563b2ea41aa4d3658bd2761
Matjaž Finžgar <matjaz@finzgar.net>
2022-12-12 13:46:39 +0100

    remove tmp

--- tmp/test.sh
+++
@@ -1,8 +0,0 @@
-ARGOCD_ENVSUBST_PLUGIN_SECRET_NAME=argocd-envsubst
-ARGOCD_ENVSUBST_PLUGIN_NAMESPACE_NAME=argocd
-
-export $(kubectl get secret -n $ARGOCD_ENVSUBST_PLUGIN_NAMESPACE_NAME $ARGOCD_ENVSUBST_PLUGIN_SECRET_NAME -o json | jq -r '.data | to_entries | map("\(.key)=\(.value|@base64d)") | .[]')
-
-#envsubst < test.yaml
-
-# helm template --release-name "$ARGOCD_APP_NAME" | envsubst
