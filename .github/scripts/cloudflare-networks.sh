fe81de874341d37fcb023b179e7d326b919a0911
Matjaž Finžgar <matjaz@finzgar.net>
2023-01-27 10:36:53 +0100

    fix: action

--- .github/scripts/cloudflare-networks.sh
+++ .github/scripts/cloudflare-networks.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-DEFAULT_CLOUDFLARE_NETWORKS_FILE="${DEFAULT_CLOUDFLARE_NETWORKS_FILE:-../../k8s/networking/traefik/templates/mw-cloudflare-only.yaml}"
-
 DATA=$(curl -s "https://api.cloudflare.com/client/v4/ips")
 export CLOUDFLARE_IPS="$(echo $DATA | yq '.result.ipv4_cidrs + .result.ipv6_cidrs')"
 
-yq -i '.spec.ipWhiteList.sourceRange |= env(CLOUDFLARE_IPS) | ..style="double"' $CLOUDFLARE_MIDDLEWARE_FILE
+yq -i '.spec.ipWhiteList.sourceRange |= env(CLOUDFLARE_IPS) | ..style="double"' $1
