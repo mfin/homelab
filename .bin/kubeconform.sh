8d68e64c814d4e471eb08246d9e69c84661b9874
Matjaž Finžgar <matjaz@finzgar.net>
2023-02-01 10:26:25 +0100

    feat: init kubeconform task

---
+++ .bin/kubeconform.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+namespace=$1
+name=$2
+
+kubernetes_version="v1.26.1"
+schema_location="https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json"
+
+# schema-location default is https://github.com/yannh/kubernetes-json-schema
+namespace=$namespace name=$name task k:template | kubeconform -strict -kubernetes-version $(echo $kubernetes_version | sed 's/v//') -skip CustomResourceDefinition -schema-location default -schema-location "$schema_location"
