1d0ad62c5076e1490926f35e017caa1fddc98d9c
Matjaž Finžgar <matjaz@finzgar.net>
2023-03-14 23:36:36 +0100

    fix: name -> app

--- .taskfiles/k8s/scripts/kubeconform.sh
+++ .taskfiles/k8s/scripts/kubeconform.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 namespace=$1
-name=$2
+app=$2
 
 # renovate: datasource=github-releases depName=kubernetes/kubernetes
 kubernetes_version="v1.26.2"
@@ -11,4 +11,4 @@ kubernetes_version="v1.26.2"
 schema_location="https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json"
 
 # schema-location default is https://github.com/yannh/kubernetes-json-schema
-namespace=$namespace name=$name task k:template | kubeconform -strict -kubernetes-version $(echo $kubernetes_version | sed 's/v//') -skip CustomResourceDefinition -schema-location default -schema-location "$schema_location"
+namespace=$namespace app=$app task k:template | kubeconform -strict -kubernetes-version $(echo $kubernetes_version | sed 's/v//') -skip CustomResourceDefinition -schema-location default -schema-location "$schema_location"
