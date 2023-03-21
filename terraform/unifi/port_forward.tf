5ca908b499febd1c3279fdafa9590ad3a7f02ea4
Matjaž Finžgar <matjaz@finzgar.net>
2023-03-21 09:00:05 +0100

    fix: unifi 3.0 finally

--- terraform/unifi/port_forward.tf
+++ terraform/unifi/port_forward.tf
@@ -17,13 +17,3 @@ resource "unifi_port_forward" "plex" {
 
   port_forward_interface = "wan"
 }
-
-resource "unifi_port_forward" "wireguard" {
-  name     = "wireguard"
-  dst_port = data.sops_file.secrets.data["forward.wireguard.port"]
-  fwd_ip   = data.sops_file.secrets.data["forward.wireguard.ip"]
-  fwd_port = data.sops_file.secrets.data["forward.wireguard.port"]
-  protocol = "tcp_udp"
-
-  port_forward_interface = "wan"
-}
