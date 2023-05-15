2a422f31dc066f1efdef0d05550505e9a5cd526d
Matjaž Finžgar <matjaz@finzgar.net>
2023-05-15 16:12:00 +0200

    fix: remove 443 from fw

--- terraform/unifi/port_forward.tf
+++ terraform/unifi/port_forward.tf
@@ -1,13 +1,3 @@
-resource "unifi_port_forward" "https" {
-  name     = "https"
-  dst_port = 443
-  fwd_ip   = data.sops_file.secrets.data["forward.https.ip"]
-  fwd_port = 443
-  protocol = "tcp"
-
-  port_forward_interface = "wan"
-}
-
 resource "unifi_port_forward" "plex" {
   name     = "plex"
   dst_port = 32400
