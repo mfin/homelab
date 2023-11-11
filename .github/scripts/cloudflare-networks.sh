73cf8ecae5d721e73b9f2911def1a53849d3fda1
Matjaž Finžgar <matjaz@finzgar.net>
2023-11-11 17:02:23 +0100

    fix:whitelist

--- .github/scripts/cloudflare-networks.sh
+++ .github/scripts/cloudflare-networks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 DATA=$(curl -s "https://api.cloudflare.com/client/v4/ips")
-export WHITELIST_IPS="$(echo $DATA | yq '.result.ipv4_cidrs + .result.ipv6_cidrs + ["10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12"] | ..style="double"' -)"
+export WHITELIST_IPS="$(echo $DATA | yq '.result.ipv4_cidrs + .result.ipv6_cidrs + ["10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12"] | join(",")' -)"
 
 yq -i '.ingress-nginx.controller.config.whitelist-source-range = env(WHITELIST_IPS)' $1
