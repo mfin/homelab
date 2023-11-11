9325689e49c850ed80a8a93a4f1665867566862d
Matjaž Finžgar <matjaz@finzgar.net>
2023-11-11 15:35:48 +0100

    fix: cloudflare ip updater

--- .github/scripts/cloudflare-networks.sh
+++ .github/scripts/cloudflare-networks.sh
@@ -3,5 +3,5 @@
 DATA=$(curl -s "https://api.cloudflare.com/client/v4/ips")
 export CLOUDFLARE_IPS="$(echo $DATA | yq '.result.ipv4_cidrs + .result.ipv6_cidrs')"
 
-yq -i '.spec.ipWhiteList.sourceRange |= env(CLOUDFLARE_IPS) | ..style="double"' $1
-yq -i '.spec.ipWhiteList.sourceRange += ["10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12"] | ..style="double"' $1
+yq -i '.ingress-nginx.controller.config.whitelist-source-range |= env(CLOUDFLARE_IPS) | ..style="double"' $1
+yq -i '.ingress-nginx.controller.config.whitelist-source-range += ["10.0.0.0/8", "192.168.0.0/16", "172.16.0.0/12"] | ..style="double"' $1
